9644117c04469e44268355e560fa01fafae4e5d6
akile <akilengyi@gmail.com>
2022-10-20 13:24:25 +0200

    yes

--- app/src/main/java/com/example/projectboardapp/MainActivity.java
+++ app/src/main/java/com/example/projectboardapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.projectboardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, saves.class));
+    }
 }
+
---
+++ app/src/main/java/com/example/projectboardapp/saves.java
@@ -0,0 +1,14 @@
+package com.example.projectboardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class saves extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saves);
+    }
+}
